20bcf96963b6ad089f31143fae77ae279e93cf08
tgaro <tiago_oa@hotmail.com>
2018-11-30 11:34:43 -0200

    Changed user schema and model

--- app/models/user.js
+++ app/models/user.js
@@ -1,26 +1,21 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
-const createModel = () => {
-	const userSchema = new schema({
-		token: {
-			type: String,
-			required: 'You must enter a valid token'
-		},
-		user: {
-			type: String,
-			required: 'Enter your username'
-			},    
-	    pass: {
-	        type: String,
-	        required: 'Enter a valid password'
-	    }
-	})
+const userSchema = new schema({
+	token: {
+		type: String,
+		required: 'You must enter a valid token'
+	},
+	user: {
+		type: String,
+		required: 'Enter your username'
+		},    
+    pass: {
+        type: String,
+        required: 'Enter a valid password'
+    }
+})
 
-	const userModel = mongoose.model('user', userSchema)
-	return userModel
-}
-
-const model = createModel()
+const model = mongoose.model('user', userSchema)
 
 module.exports = {model}
