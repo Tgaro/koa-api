802fba296698afedffee25dfa5967f552ca7649c
tgaro <tiago_oa@hotmail.com>
2018-12-03 17:48:09 -0200

    Created file only for mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const app = require('./config/app')
+const app = require('./src/app')
+const db = require('./src/db/dbConnection')
 const port = 3031
+const url = 'mongodb://localhost/koa-apiDB'
+
+db.connect(url)
 const server = app.listen(port, () => {console.log(`Server's listening on port ${port}`)})
 
 module.exports = server
